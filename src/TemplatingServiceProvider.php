148b1b489e86cdd87172337728c3213f700368f3
Humayun Shahzad <humayun.shahzad@pitb.gov.pk>
2026-02-23 15:41:17 +0500

    package init...

--- src/TemplatingServiceProvider.php
+++ src/TemplatingServiceProvider.php
@@ -13,21 +13,19 @@ class TemplatingServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        // Publish config
-        // $this->publishes([
-        //     __DIR__.'/../config/templating.php' => config_path('templating.php'),
-        // ], 'templating-config');
-
         $this->publishes([
-        // layout
-        __DIR__.'/../resources/views/layouts/masterTemplatePage.blade.php' => resource_path('views/layouts/masterpage.blade.php'),
 
-        __DIR__.'/../resources/views/layouts/loginTemplatePage.blade.php' => resource_path('views/layouts/loginpage.blade.php'),
+            // Layouts
+            __DIR__.'/../resources/views/layouts/masterTemplatePage.blade.php'
+                => resource_path('views/layouts/masterpage.blade.php'),
 
-        __DIR__.'/../public/assets' => public_path('public/assets'),
+            __DIR__.'/../resources/views/layouts/loginTemplatePage.blade.php'
+                => resource_path('views/layouts/loginpage.blade.php'),
+
+            // Assets
+            __DIR__.'/../public/assets'
+                => public_path('public/assets'),
 
         ], 'templating');
-
-        
     }
 }
